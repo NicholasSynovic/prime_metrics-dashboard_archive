5d303070e834b0044cf0ff15f21296a31e8fb322
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-09-22 15:30:59 -0500

    Fix module to export data from a volume

--- Commits/commitsModule.sh
+++ Commits/commitsModule.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # Get cwd
-DIR="$(pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create docker volume
 docker volume create metrics
 
 # Build and run docker volume, then get the container ID of the last ran container
-docker build . -t code
-docker run -v metrics:/metrics code $1 $2
+docker build -t commits .
+docker run -v metrics:/metrics commits $1 $2
 
 CONTAINERID="$(docker ps -q -n 1)"
+
+echo $CONTAINERID
 
 # Copy volume data to the cwd
 # TODO: Fix this so that it actually copies data
