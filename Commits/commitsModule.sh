3157137d9f31edd0cebf5ef8c0d4ddc16b5e7715
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-09-22 17:50:40 -0500

    Finish writing commitsModule.sh

--- Commits/commitsModule.sh
+++ Commits/commitsModule.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
+# IF statements from https://stackoverflow.com/a/6482403
+
+if [ $# -eq 0 ]
+  then
+    echo "No URL, Personal Access Token, or Export Location arguments supplied."
+    exit
+fi
+
+if [ -z "$1" ]
+  then
+    echo "No URL argument supplied."
+    exit
+fi
+
+if [ -z "$2" ]
+  then
+    echo "No Personal Access Token argument supplied."
+    exit
+fi
+
+
+if [ -z "$3" ]
+  then
+    echo "No Export Location argument supplied."
+    exit
+fi
+
 # Set VOLUMENAME
 VOLUMENAME="commits"
-
-# Get CWD
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create docker volume
 docker volume create $VOLUMENAME
@@ -16,7 +40,7 @@ docker run -v $VOLUMENAME:/metrics code $1 $2
 CONTAINERID="$(docker ps -q -n 1)"
 
 # Copy volume data to the CWD
-docker container cp $CONTAINERID:/metrics $DIR
+docker container cp $CONTAINERID:/metrics $3
 
 # Remove created docker containers and volumes after the container has been stopped
 echo "Stopping docker container" $CONTAINERID
@@ -29,6 +53,6 @@ echo "Deleting volume with NAME:" $VOLUMENAME
 docker volume rm $VOLUMENAME
 
 # echo "Deleting ALL DOCKER CONTAINERS AND VOLUMES"
-docker system prune -a --volumes
+# docker system prune -a --volumes
 
 echo "Metrics created and stored in" $DIR"/metrics"
