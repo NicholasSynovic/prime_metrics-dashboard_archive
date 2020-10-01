ed465a40edb3b2c1b2b9d86de0a771926c113ea8
svonhatten <svonhatten@luc.edu>
2020-09-30 21:34:56 -0500

    update fast_metrics.sh file

--- fast_metrics.sh
+++ fast_metrics.sh
@@ -4,29 +4,34 @@ echo "Running all metrics scripts for repository: $1"
 
 docker volume create metrics
 
+# Run Commits
+cd Commits
+./CommitsModule.sh $1 $2
+cd ..
+
 # Run Issues
 cd Issues
-./metrics.sh $1 $2
+./IssuesModule.sh $1 $2
 cd ..
 
 # Run GitModule
 cd GitModule
-./metrics.sh $1
+./GitModule.sh $1
 cd ..
 
 # Run Issue_Spoilage
 cd Issue_Spoilage
-./metrics.sh $1 $2
+./IssueSpoilageModule.sh $1 $2
 cd ..
 
 # Run Defect Density (this command should also copy volume content to the current dir)
 cd Defect_Density
-./metrics.sh $1 $2
+./DefectDensityModule.sh $1 $2
 cd ..
 
 # Run Graph
 cd Graph
-./metrics.sh $1
+./GraphModule.sh $1
 cd ..
 
 
