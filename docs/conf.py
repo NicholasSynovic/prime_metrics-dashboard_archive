f9afb05574dd012afe7bf312742b4c50ac292dd5
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-10-11 15:56:08 -0500

    Add the sphinx documentation

---
+++ docs/conf.py
@@ -0,0 +1,261 @@
+# Project Information
+project = "SSL Metrics Dashboard"
+copyright = "2020, Software and Systems Laboratory"
+author = "Software and Systems Laboratory"
+version = "0.01"
+release = "alpha"
+
+# General Configuration
+extensions = ["sphinx.ext.todo", "sphinx.ext.autosectionlabel"]
+source_suffix = {".rst": "restructuredtext"}
+source_encoding = "utf-8-sig"
+# source_parsers =
+master_doc = "index"
+# exclude_patterns = []
+templates_path = ["_templates"]
+# template_bridge =
+# rst_epilog = """ """
+# rst_prolog = """ """
+primary_domain = "py"
+# default_role =
+keep_warnings = False
+# suppress_warnings = []
+# needs_sphinx =
+# needs_extensions = {}
+# manpages_url =
+nitpicky = True
+# nitpick_ignore =
+numfig = False
+# numfig_format =
+# numfig_secnum_depth =
+smartquotes = True  # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-smartquotes
+# smartquotes_action = ""
+# smartquotes_excludes = {}
+# user_agent =
+tls_verify = True
+# tls_cacerts = ""
+# today =
+# today_fmt =
+highlight_language = "python3"
+# highlight_options =
+# pygments_style
+# add_function_parentheses
+# add_module_names
+# show_authors
+# modindex_common_prefix
+# trim_footnote_reference_space
+# trim_doctest_flags
+# strip_signature_backslash
+
+# Options for Internationalization
+language = "en"
+# locale_dirs =
+# gettext_compact
+# gettext_uuid
+# gettext_location
+# gettext_auto_build
+# gettext_additional_targets
+# figure_language_filename
+
+# Options for Math
+math_number_all = False
+# math_eqref_format
+# math_eqref_format
+# math_eqref_format
+
+# Options for HTML Output
+
+# html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
+html_theme_options = {
+    "nosidebar": True,
+    # "sidebarwidth"
+    # "body_min_width"
+    # "body_max_width"
+    "navigation_with_keys": True,
+    # "globaltoc_collapse"
+    # "globaltoc_includehidden"
+    # "globaltoc_maxdepth"
+    # Sphinx_RTD_Theme Options
+    # "canonical_url": "",
+    # "analytics_id": "UA-XXXXXXX-1",  #  Provided by Google in your dashboard
+    # "logo_only": False,
+    "display_version": False,
+    "prev_next_buttons_location": "None",
+    "style_external_links": True,
+    # "vcs_pageview_mode": "blob",
+    "style_nav_header_background": "maroon",
+    # Sphinx_RTD_Theme TOCTREE Options
+    # "collapse_navigation": True,
+    # "sticky_navigation": True,
+    # "navigation_depth": 4,
+    # "includehidden": True,
+    "titles_only": True,
+}
+github_url = "https://github.com/SoftwareSystemsLaboratory/Metrics-Dashboard"
+# html_style =
+html_title = "SSL Metrics Dashboard"
+html_short_title = "Metrics Dashboard"
+# html_baseurl
+# html_codeblock_linenos_style
+# html_context
+# html_logo
+# html_favicon
+# html_css_files
+# html_js_files
+html_static_path = ["_static"]
+# html_extra_path
+# html_last_updated_fmt
+# html_use_smartypants
+# html_add_permalinks
+# html_sidebars
+# html_additional_pages
+# html_domain_indices
+# html_use_index
+# html_split_index
+# html_copy_source
+# html_sourcelink_suffix
+# html_use_opensearch
+# html_file_suffix
+# html_link_suffix
+# html_show_copyright
+# html_show_sphinx
+# html_output_encoding
+# html_compact_lists
+# html_secnumber_suffix
+# html_search_language
+# html_search_options
+# html_search_scorer
+# html_scaled_image_link
+# html_math_renderer
+# html4_writer
+
+# Options for Single HTML Output
+# singlehtml_sidebars
+
+# Options for HTML Help Output
+# htmlhelp_basename
+# htmlhelp_file_suffix
+# htmlhelp_link_suffix
+
+# Options for Apple Help Output
+# applehelp_bundle_name
+# applehelp_bundle_id
+# applehelp_dev_region
+# applehelp_bundle_version
+# applehelp_icon
+# applehelp_kb_product
+# applehelp_kb_url
+# applehelp_remote_url
+# applehelp_index_anchors
+# applehelp_min_term_length
+# applehelp_stopwords
+# applehelp_locale
+# applehelp_title
+# applehelp_codesign_identity
+# applehelp_codesign_flags
+# applehelp_indexer_path
+# applehelp_codesign_path
+# applehelp_disable_external_tools
+
+# Options for EPUB Output
+# epub_basename
+# epub_theme
+# epub_theme_options
+# epub_title
+# epub_description
+# epub_author
+# epub_contributor
+# epub_language
+# epub_publisher
+# epub_copyright
+# epub_identifier
+# epub_scheme
+# epub_uid
+# epub_cover
+# epub_css_files
+# epub_guide
+# epub_pre_files
+# epub_post_files
+# epub_exclude_files
+# epub_tocdepth
+# epub_tocdup
+# epub_tocscope
+# epub_fix_images
+# epub_max_image_width
+# epub_show_urls
+# epub_use_index
+# epub_writing_mode
+
+# Options for LaTeX Output
+# latex_engine
+# latex_documents
+# latex_logo
+# latex_toplevel_sectioning
+# latex_appendices
+# latex_domain_indices
+# latex_show_pagerefs
+# latex_show_urls
+# latex_use_latex_multicolumn
+# latex_use_xindy
+# latex_elements
+# latex_docclass
+# latex_additional_files
+# latex_theme
+# latex_theme_options
+# latex_theme_path
+
+# Options for Text Output
+# text_newlines
+# text_sectionchars
+# text_add_secnumbers
+# text_secnumber_suffix
+
+# Options for Manual Page Output
+# man_pages
+# man_show_urls
+# man_make_section_directory
+
+# Options for Texinfo Output
+# texinfo_documents
+# texinfo_appendices
+# texinfo_domain_indices
+# texinfo_show_urls
+# texinfo_no_detailmenu
+# texinfo_elements
+
+# Options for QtHelp Output
+# qthelp_basename
+# qthelp_namespace
+# qthelp_theme
+# qthelp_theme_options
+
+# Options for the LinkCheck Builder
+# linkcheck_ignore
+# linkcheck_request_headers
+# linkcheck_retries
+# linkcheck_timeout
+# linkcheck_workers
+# linkcheck_anchors
+# linkcheck_anchors_ignore
+# linkcheck_auth
+
+# Options for the XML Builder
+# xml_pretty
+
+# Options for the C Domain
+# c_id_attributes
+# c_paren_attributes
+# c_allow_pre_v3
+# c_warn_on_allowed_pre_v3
+
+# Options for the C++ Domain
+# Options for the C++ domain
+# cpp_id_attributes
+# cpp_paren_attributes
+
+# sphinx.ext.todo Configuration
+todo_include_todos = True
+
+# sphinx.ext.autosectionlabel Configuration
+autosectionlabel_prefix_document = True
