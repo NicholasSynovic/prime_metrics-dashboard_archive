2087fbd719d0177cd68d664e7e8e3a37a44f4f58
eamobi <amobisomto@gmail.com>
2020-10-26 17:16:23 -0500

    add docs

--- Issues/metrics/IssuesCalculation.py
+++ Issues/metrics/IssuesCalculation.py
@@ -2,9 +2,17 @@ import sqlite3
 #TODO: ADD Documentation later
 class Calculations:
     def __init__(self, db_file):
+        ''' 
+        Initialize class variables
+        :param db_file: The db file to be used by the class
+        '''
         self.conn = self.create_connection(db_file)
 
     def create_connection(self,db_file):
+        ''' 
+        Creates a connection with the db
+        :param db_file: The db file to create connection with
+        '''
         conn = None 
         try: 
             conn = sqlite3.connect(db_file)
@@ -14,6 +22,10 @@ class Calculations:
         return conn
 
     def get_total_issues(self,conn):
+        ''' 
+        Returns the total number of issues
+        :param conn: The db connection
+        '''
         cur = conn.cursor()
         cur.execute("SELECT COUNT(*) from ISSUES ")
         result = cur.fetchall()[0][0]
@@ -21,6 +33,12 @@ class Calculations:
         return result
 
     def count_rows_by_col_value(self,col,value,conn):
+        ''' 
+        counts rows filtered by given column and specified value
+        :param col: The column name
+        :param value: The value for filtering
+        :param conn: The db connection
+        '''
         cur = conn.cursor()
         cur.execute("SELECT COUNT(*) {columnS} from ISSUES where {columnS}='{valueS}'".format(valueS=value,columnS=col))
         result = cur.fetchall()[0][0] # get first item in tuples
@@ -28,21 +46,36 @@ class Calculations:
         return result
 
     def get_open_count(self,conn):
+        ''' 
+        Returns the total number of open issues
+        :param conn: The db connection
+        '''
         open_count = self.count_rows_by_col_value("state","open",conn)
         return open_count
 
     def get_closed_count(self,conn):
+        ''' 
+        Returns the total number of closed issues
+        :param conn: The db connection
+        '''
         closed_count = self.count_rows_by_col_value("state","closed",conn)
         return closed_count
 
     def get_closed_to_open_ratio(self,conn):
+        ''' 
+        Returns the ratio of closed to opened issues
+        :param conn: The db connection
+        '''
         open_count = self.get_open_count(conn)
         closed_count = self.get_closed_count(conn)
         ratio = round((closed_count/ open_count),2)
         return ratio
 
     def get_closing_efficiency(self,conn):
-        ''' '''
+        ''' 
+        Based on the idea of work efficiency, efficiency = output/input, where output = closed issues, input = total issues
+        :param conn: The db connection
+        '''
         total = self.get_total_issues(conn)
         closed_count = self.get_closed_count(conn)
         closing_efficiency = round((closed_count / total),2)
