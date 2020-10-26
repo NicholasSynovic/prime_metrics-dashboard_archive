b17a068edc97ddc868a64ca6694eac3850dd24f4
eamobi <amobisomto@gmail.com>
2020-10-26 16:06:46 -0500

    Initial class and functions  for calcluations

--- Issues/metrics/IssuesCalculation.py
+++ Issues/metrics/IssuesCalculation.py
@@ -1,6 +1,42 @@
 import sqlite3 
-import pandas as pd
-dbFile = "alt-tab-macos.db"
-conn = sqlite3.connect(dbFile)
-df = pd.read_sql_query("select state from ISSUES", conn)
-print(df)
+# import pandas as pd
+db_file = "alt-tab-macos.db"
+conn = sqlite3.connect(db_file)
+# decide if to use sql or pandas for the calculations
+# df = pd.read_sql_query("select state from ISSUES", conn)
+#TODO: ADD Documentation later
+cur = conn.cursor()
+cur.execute("SELECT COUNT(*) state from ISSUES where state='closed'")
+
+print(cur.fetchall()[0][0])
+
+class Calculations:
+    def __init__(self, db_file):
+        # self.db_file = db_file
+        self.conn = self.create_connection(db_file)
+
+    def create_connection(self,db_file):
+        conn = None 
+        try: 
+            conn = sqlite3.connect(db_file)
+        except Error as e: 
+            print(e)
+
+        return conn
+
+    def count_rows_by_col_value(self,col,value,conn):
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) {columnS} from ISSUES where {columnS}='{valueS}'".format(valueS=value,columnS=col))
+        result = cur.fetchall()[0][0] # get first item in tuples
+        return result
+    def get_open_count(self,conn):
+        open_count = self.count_rows_by_col_value("state","open",conn)
+        return open_count
+
+#test
+A = Calculations("alt-tab-macos.db")
+print(A.get_open_count(A.conn))
+
+        
+
+
