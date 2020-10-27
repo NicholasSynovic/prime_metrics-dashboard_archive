d896063eb8111702e8e9175917dc13dd21e84ba6
eamobi <amobisomto@gmail.com>
2020-10-27 18:23:12 -0500

    added more tests

--- Issues/metrics/testCalculation.py
+++ Issues/metrics/testCalculation.py
@@ -12,6 +12,32 @@ class TestIssuesCalculation(unittest.TestCase):
         result = self.testCalc.get_total_issues(self.conn)
         self.assertTrue(expected == result)
 
+    def test_get_open_count(self):
+        expected = 54 
+        result = self.testCalc.get_open_count(self.conn)
+        self.assertTrue(expected == result)
+
+    def test_get_closed_count(self):
+        expected = 585
+        result = self.testCalc.get_closed_count(self.conn)
+        self.assertTrue(expected == result)
+
+    def test_get_closed_to_open_ratio(self):
+        expected = 10.83
+        result = self.testCalc.get_closed_to_open_ratio(self.conn)
+        self.assertTrue(expected == result)
+
+    def test_get_closing_efficiency(self):
+        expected = "92.0%"
+        result = self.testCalc.get_closing_efficiency(self.conn)
+        self.assertTrue(expected == result)
+
+    def test_get_avg_to_close_issue(self):
+        expected = 15.95
+        result = self.testCalc.get_avg_days_to_close_issue(self.conn)
+        self.assertTrue(expected == result)
+
+
 if __name__ == '__main__':
     unittest.main()
         
