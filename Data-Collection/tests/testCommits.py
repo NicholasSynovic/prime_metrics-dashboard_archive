6f5031d3d3b8d391ee8e64240b7402f91b39cb0e
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-11-08 12:23:06 -0600

    Fix the testCommits.py file to setup and tear down

--- Data-Collection/tests/testCommits.py
+++ Data-Collection/tests/testCommits.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import os
 import unittest
 from json import load
 
@@ -12,41 +13,44 @@ from libs.databaseConnector import DatabaseConnector
 from libs.githubConnector import GitHubConnector
 
 
-dbConnection = DatabaseConnector(databaseFileName="temp.db")
-dbConnection.createDatabase()
-dbConnection.openDatabaseConnection()
-commitsSQL = "CREATE TABLE Commits (SHA TEXT,Commit_Date TEXT, Author TEXT, Message TEXT, Comment_Count INTEGER, PRIMARY KEY(SHA));"
-dbConnection.executeSQL(sql=commitsSQL, commit=True)
-
-with open("jsonResponses.json", "r") as file:
-    commitsResponse = load(file)["commits"]
-    file.close()
-
-with open("jsonResponseHeaders.json", "r") as file:
-    commitsResponseHeaders = load(file)["commits"]
-    file.close()
-
-commitsCollector = Commits(
-    dbConnection=dbConnection,
-    oauthToken="CHANGE ME",
-    repository="Metrics-Dashboard",
-    username="SoftwareSystemsLaboratory",
-)
-
-
 class TestCommits(unittest.TestCase):
+    def setUp(self) -> None:
+        self.dbConnection = DatabaseConnector(databaseFileName="temp.db")
+        self.dbConnection.createDatabase()
+        self.dbConnection.openDatabaseConnection()
+        commitsSQL = "CREATE TABLE Commits (SHA TEXT,Commit_Date TEXT, Author TEXT, Message TEXT, Comment_Count INTEGER, PRIMARY KEY(SHA));"
+        self.dbConnection.executeSQL(sql=commitsSQL, commit=True)
+
+        with open("jsonResponses.json", "r") as file:
+            self.commitsResponse = load(file)["commits"]
+            file.close()
+
+        with open("jsonResponseHeaders.json", "r") as file:
+            self.commitsResponseHeaders = load(file)["commits"]
+            file.close()
+
+        self.commitsCollector = Commits(
+            dbConnection=self.dbConnection,
+            oauthToken="CHANGE ME",
+            repository="Metrics-Dashboard",
+            username="SoftwareSystemsLaboratory",
+        )
+
     def test_Commits(self):
-        assert commitsCollector.currentPage == 1
-        assert commitsCollector.repository == "Metrics-Dashboard"
-        assert commitsCollector.username == "SoftwareSystemsLaboratory"
+        assert self.commitsCollector.currentPage == 1
+        assert self.commitsCollector.repository == "Metrics-Dashboard"
+        assert self.commitsCollector.username == "SoftwareSystemsLaboratory"
 
     def test_getData(self):
-        data = commitsCollector.getData()
+        data = self.commitsCollector.getData()
         assert data[1].status_code == 200
         self.assertIsInstance(data[0], list)
 
     def test_insertData(self):
-        commitsCollector.insertData(dataset=commitsResponse)
+        self.commitsCollector.insertData(dataset=self.commitsResponse)
 
     # TODO: Create a proper test for this function
     # def test_IterateNext(self):
+
+    def tearDown(self) -> None:
+        os.remove("temp.db")
