ebc64cdc8f5d6c70a9862a1d33fee7ed0c807c9b
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-11-08 12:16:20 -0600

    Create the test file for commits.py

--- Data-Collection/tests/testCommits.py
+++ Data-Collection/tests/testCommits.py
@@ -1,15 +1,52 @@
 import json
+import sys
 import unittest
 from json import load
-from unittest.case import TestCase
 
-from . import Commits
+from requests import Response
+
+sys.path.append("../")
+
+from commits import Commits
+from libs.databaseConnector import DatabaseConnector
+from libs.githubConnector import GitHubConnector
+
+
+dbConnection = DatabaseConnector(databaseFileName="temp.db")
+dbConnection.createDatabase()
+dbConnection.openDatabaseConnection()
+commitsSQL = "CREATE TABLE Commits (SHA TEXT,Commit_Date TEXT, Author TEXT, Message TEXT, Comment_Count INTEGER, PRIMARY KEY(SHA));"
+dbConnection.executeSQL(sql=commitsSQL, commit=True)
+
+with open("jsonResponses.json", "r") as file:
+    commitsResponse = load(file)["commits"]
+    file.close()
+
+with open("jsonResponseHeaders.json", "r") as file:
+    commitsResponseHeaders = load(file)["commits"]
+    file.close()
+
+commitsCollector = Commits(
+    dbConnection=dbConnection,
+    oauthToken="18c3c5e0ae7336feed264704c0946b6f80fc7f31",
+    repository="Metrics-Dashboard",
+    username="SoftwareSystemsLaboratory",
+)
 
 
 class TestCommits(unittest.TestCase):
-    def __init__(self) -> None:
-        with open("jsonResponses.json", "r") as file:
-            self.commitsResponse = load(file)["commits"]
-            file.close()
+    def test_Commits(self):
+        assert commitsCollector.currentPage == 1
+        assert commitsCollector.repository == "Metrics-Dashboard"
+        assert commitsCollector.username == "SoftwareSystemsLaboratory"
 
-        self.commitsCollector = Commits()
+    def test_getData(self):
+        data = commitsCollector.getData()
+        assert data[1].status_code == 200
+        self.assertIsInstance(data[0], list)
+
+    def test_insertData(self):
+        commitsCollector.insertData(dataset=commitsResponse)
+
+    # TODO: Create a proper test for this function
+    # def test_IterateNext(self):
