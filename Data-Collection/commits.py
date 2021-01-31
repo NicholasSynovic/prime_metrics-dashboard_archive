61c52d549ce4f1db3b075c39c580bc5e8718bc3c
ErikCG <50557869+ErikCG@users.noreply.github.com>
2021-01-31 22:22:57 +0000

    added documentation

--- Data-Collection/commits.py
+++ Data-Collection/commits.py
@@ -2,7 +2,25 @@ from libs.collector import Collector_4
 
 
 class Commits(Collector_4):
+    """Handels data referring to commits. Inherits from Collector_4"""
+
     def insertData(self, dataset: dict) -> None:
+        """ Takes in data identifying commits and inserts it into the database.
+    
+        Iterates through the dataset, assigns an id num and executes sql to insert data into 
+        the database in a for loop. Will ignore the data if it is duplicated.
+
+        Parameters:
+            dataset (dict): nested dictionary containing data to be inserted into database
+
+        Note:
+            dataset should include: the commit hash, author's name, commit date, tree hash 
+            and comment count 
+
+        Returns:
+            No return value
+        """
+        
         id = 0
         for dataPoint in range(len(dataset)):
             sha = dataset[dataPoint]["sha"]
--- Data-Collection/issues.py
+++ Data-Collection/issues.py
@@ -2,10 +2,40 @@ from libs.collector import Collector_3
 
 
 class Issues(Collector_3):
+    """Handels data referring to issues. Inherits from Collector_3 class"""
+
     def insertData(self, dataset: dict) -> None:
+        """ Takes in data identifying commits and inserts it into the database.
+    
+        Iterates through the dataset and executes sql to insert required data into the database in a 
+        for loop. Will ignore the data if it is duplicated.
+
+        Parameters:
+            dataset (dict): nested dictionary containing data to be inserted into database
+        
+        Note:
+            dataset should include: id num, count, issue title, author name, assignees, labels, 
+            creation date, update date, closed date.
+
+        Returns:
+            No return value
+        """
+
         def _labelCollection(
             index: int,
         ) -> str:
+            """Joins labels together into one string
+
+            Uses a for loop to take labels from dataset and append them to an array. 
+            Then uses .join() to join on the array to create one string and return.
+
+            Paramaters:
+                index (int): index num of entry in dataset to pull labels from
+
+            Returns:
+                str: string of labels joined together with ', '
+            """
+            
             labelNames = []
             for label in dataset[index]["labels"]:
                 labelNames.append(label["name"])
@@ -14,6 +44,18 @@ class Issues(Collector_3):
         def _asigneeCollection(
             index: int,
         ) -> str:
+            """Joins asignees together into one string
+
+            Uses a for loop to take asignees from dataset and append them to an arry.
+            Then uses .join() on the array to create one string and return.
+
+            Paramaters:
+                index (int): index num of entry in dataset to pull asignees from
+
+            Returns:
+                str: string of asignees joined together with ', '
+            """
+
             labelNames = []
             for assignee in dataset[index]["assignees"]:
                 labelNames.append(assignee["login"])
