239523a86b3747289df60e12c6095a820aa9310e
eamobi <amobisomto@gmail.com>
2021-01-31 16:15:30 -0600

    timing function

--- Data-Collection/dataCollection.py
+++ Data-Collection/dataCollection.py
@@ -1,6 +1,6 @@
 from sqlite3 import Connection
 from tqdm import tqdm
-
+from datetime import datetime as dt # used for logging time
 from branches import Branches
 from commits import Commits
 from files import Files
@@ -190,6 +190,7 @@ class DataCollection:
 
 
 if __name__ == "__main__":
+    start = dt.now()#start time
     cmdLineArgs = arguementHandling()
 
     dc = DataCollection(
@@ -200,3 +201,5 @@ if __name__ == "__main__":
     )
 
     dc.startDataCollection()
+    end = dt.now()
+    print("Completed in: {}".format(end-start))
